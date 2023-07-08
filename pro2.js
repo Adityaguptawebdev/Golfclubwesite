2583a63cb45770800fcd53dc51474b9f76634dbc
ADITYA GUPTA <123538200+Adityaguptawebdev@users.noreply.github.com>
2023-07-08 18:01:29 +0530

    Add files via upload

---
+++ pro2.js
@@ -0,0 +1,26 @@
+
+
+gsap.to("#nav",{
+backgroundColor:"#000",
+duration:0.5,
+Height:"110px",
+scrollTrigger:{
+    trigger:"#nav",
+    scroller:"body",
+markers:true,
+start:"top -10%",
+end:"top -11%",
+scrub:1
+}
+})
+gsap.to("#main",{
+    backgroundColor:"#000",
+    scrollTrigger:{
+        trigger:"#main",
+        scroller:"body",
+        markers:true,
+        start:" top-20%",
+        end:"top -70%",
+        scrub:2
+    }
+})
